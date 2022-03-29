5c2c1b0c23e8a3d081c9fad3ccf53850c4ba05e1
CostasAK <costas@kokke.eu>
2022-03-29 16:40:41 +0200

    set test to header of example page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+import App from "./App";
+
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/React Components for Ko-fi/i);
   expect(linkElement).toBeInTheDocument();
 });
