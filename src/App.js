fabc268e6d6544beb1ae486ff33f98e4f6933e1a
CostasAK <costas@kokke.eu>
2022-03-31 11:03:07 +0200

    changed subheader to description

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="Header">
         <h1>react-kofi-button</h1>
-        <h2>Examples of the different components, presets and properties</h2>
+        <h2>React Components to link to your Ko-fi profile</h2>
       </header>
       <main className="Main">
         <KofiButton username="costasak" label="Default preset" />
