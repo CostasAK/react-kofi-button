adb1a3f447b8b6599d7650530a85b90b258d66a8
CostasAK <costas@kokke.eu>
2022-03-31 14:13:28 +0200

    added social_preview for opengraph

--- src/App.js
+++ src/App.js
@@ -5,27 +5,57 @@ import { KofiButton } from "./components";
 function App() {
   return (
     <div className="App">
-      <header className="Header">
+      <section className="Examples">
         <h1>react-kofi-button</h1>
-        <h2>React Components to link to your Ko-fi profile</h2>
-      </header>
-      <main className="Main">
-        <KofiButton username="costasak" label="Default preset" />
-        <KofiButton
-          username="costasak"
-          label="Thin preset"
-          preset="thin"
-          backgroundColor="kofiRed"
-          animation="on_hover"
-        />
-        <KofiButton
-          username="costasak"
-          label="Skinny preset"
-          preset="skinny"
-          backgroundColor="kofiGrey"
-          animation={false}
-        />
-      </main>
+        <h2>Examples for the README</h2>
+        <figure>
+          <KofiButton username="costasak" label="Default preset" />
+          <KofiButton
+            username="costasak"
+            label="Thin preset"
+            preset="thin"
+            backgroundColor="kofiRed"
+            animation="on_hover"
+          />
+          <KofiButton
+            username="costasak"
+            label="Skinny preset"
+            preset="skinny"
+            backgroundColor="kofiGrey"
+            animation={false}
+          />
+        </figure>
+      </section>
+      <section className="SocialPreviewContainer">
+        <h1>Social Preview</h1>
+        <div className="SocialPreviewBox">
+          <section className="SocialPreview">
+            <h1>react-kofi-button</h1>
+            <h2>React Components to link to your Ko-fi profile</h2>
+            <figure>
+              <KofiButton
+                username="costasak"
+                label="Support Me"
+                animation={false}
+              />
+              <KofiButton
+                username="costasak"
+                label="Donate"
+                preset="thin"
+                backgroundColor="kofiRed"
+                animation={false}
+              />
+              <KofiButton
+                username="costasak"
+                label="Buy me a coffee"
+                preset="skinny"
+                backgroundColor="kofiGrey"
+                animation={false}
+              />
+            </figure>
+          </section>
+        </div>
+      </section>
     </div>
   );
 }
